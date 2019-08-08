cc27f79bcfcda7f77481c4b6c44b24a22fad6d5a
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 07:51:52 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,22 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  while 'takes in an array and a value and returns the index of that value' do
+    array_methods.include? ("4")
+
+end
+
+  while 'if the value is not found, returns nil' do
+    array_methods.include? ("10")
+
 end
 
 def find_max_value(array)
-  # Add your solution here
+  when 'takes in an array of integers and returns the highest value integer' do
+    int max = array_methods
+
 end
 
 def find_min_value(array)
-  # Add your solution here
+  while 'takes in an array of integers and returns the lowest value integer' do
+    int min = array_methods
+
 end
