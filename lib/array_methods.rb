3a6a108d032bc1b51447eccb6cd7f053ced8b579
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 07:51:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -16,5 +16,7 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  while 'takes in an array of integers and returns the lowest value integer' do
+    int min = array_methods
+  
 end
